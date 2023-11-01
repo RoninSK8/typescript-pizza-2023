e0c662291508676bc5481019bb2235c0b19bb441
Сергей Политаев <ronin@sonikelf.ru>
2023-11-01 07:38:25 +0300

    Add first component - button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import './Button.css';
+import { ButtonProps } from './Button.props';
+
+function Button({ children, ...props }: ButtonProps) {
+	return (
+		<button className="button accent" {...props}>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
