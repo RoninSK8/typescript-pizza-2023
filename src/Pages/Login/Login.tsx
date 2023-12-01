b40e6fd052ffdb8d54991e6339ebe5dc74bd65e5
Сергей Политаев <ronin@sonikelf.ru>
2023-12-01 22:50:41 +0300

    Implement getting access token from backend

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -3,16 +3,48 @@ import styles from './Login.module.css';
 import Header from '../../components/Header/Header';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import { PREFIX } from '../../helpers/API';
+import axios, { AxiosError } from 'axios';
+
+export type LoginForm = {
+	email: {
+		value: string;
+	};
+	password: {
+		value: string;
+	};
+};
 
 function Login() {
-	const submit = (e: FormEvent) => {
-		e.preventDefault();
-		console.log(e);
+	const [error, setError] = useState<string | null>();
+	const sendLogin = async (email: string, password: string) => {
+		try {
+			const { data } = await axios.post(`${PREFIX}/auth/login`, {
+				email,
+				password,
+			});
+			console.log(data);
+		} catch (e) {
+			if (e instanceof AxiosError) {
+				console.log(e);
+				setError(e.response?.data.message);
+			}
+		}
 	};
+
+	const submit = async (e: FormEvent) => {
+		e.preventDefault();
+		setError(null);
+		const target = e.target as typeof e.target & LoginForm;
+		const { email, password } = target;
+		await sendLogin(email.value, password.value);
+	};
+
 	return (
 		<div className={styles['login']}>
 			<Header>Вход</Header>
+			{error && <div className={styles['error']}>{error}</div>}
 			<form className={styles['form']} onSubmit={submit}>
 				<div className={styles['field']}>
 					<label htmlFor="email">Ваш Email</label>
