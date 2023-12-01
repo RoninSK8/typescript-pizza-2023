03e064d32cfdce51eddbaf37c85b7a7384b69e1f
Сергей Политаев <ronin@sonikelf.ru>
2023-12-01 07:24:04 +0300

    Fix styles - add cn input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,7 +10,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
 	return (
 		<input
 			ref={ref}
-			className={cn(className, {
+			className={cn(styles['input'], className, {
 				[styles['invalid']]: isValid,
 			})}
 			{...props}
