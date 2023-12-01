8fa20d4fdd54f2d941e197e4abfd6b4e71039de0
Сергей Политаев <ronin@sonikelf.ru>
2023-12-01 07:39:46 +0300

    Add login page

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,5 +1,41 @@
+import { Link } from 'react-router-dom';
+import styles from './Login.module.css';
+import Header from '../../components/Header/Header';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import { FormEvent } from 'react';
+
 function Login() {
-	return <div>Login</div>;
+	const submit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(e);
+	};
+	return (
+		<div className={styles['login']}>
+			<Header>Вход</Header>
+			<form className={styles['form']} onSubmit={submit}>
+				<div className={styles['field']}>
+					<label htmlFor="email">Ваш Email</label>
+					<Input id="email" name="email" placeholder="Email"></Input>
+				</div>
+				<div className={styles['field']}>
+					<label htmlFor="password">Ваш пароль</label>
+					<Input
+						id="password"
+						name="password"
+						placeholder="Пароль"
+						type="password"
+					></Input>
+				</div>
+
+				<Button appearence="big">ВХОД</Button>
+			</form>
+			<div className={styles['links']}>
+				<div>Нет аккаунта?</div>
+				<Link to="/auth/register">Зарегистрироваться</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
